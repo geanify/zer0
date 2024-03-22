327d25665453bdca3a4846ecfc2758e19948d4d2
geanify <geanify@gmail.com>
2024-03-22 20:14:00 +0200

    switching to go, fuck it

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/geanify/zer0
+
+go 1.23
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "./Component"
-import { StateSingleton } from "./State"
-
-const comp = new Component()
-new StateSingleton([comp])
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = comp.onRender()
